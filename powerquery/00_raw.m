680bf8c57de1605a82a743d2c087219a0316bebe
NJ3412 <122598074+NeelamJethoe@users.noreply.github.com>
2026-03-30 10:29:20 +0200

    Added safe datetime conversion and filtered invalid check-ins in 00_raw

--- powerquery/00_raw.m
+++ powerquery/00_raw.m
@@ -2,6 +2,10 @@ let
   Source = Excel.Workbook(File.Contents(pFilePath), null, true),
   #"Navigation 1" = Source{[Item = "202501", Kind = "Sheet"]}[Data],
   #"Promoted headers" = Table.PromoteHeaders(#"Navigation 1", [PromoteAllScalars = true]),
-  #"Changed column type" = Table.TransformColumnTypes(#"Promoted headers", {{"CustomerID", type text}, {"Leeftijd", Int64.Type}, {"Abonnement / addon", type text}, {"Binnen geweest op", type datetime}, {"Poort", type text}, {"Ratio", type text}}, "en-US")
+  #"Converted Binnen safely" = Table.TransformColumns(
+      #"Promoted headers",
+      {{"Binnen geweest op", each try DateTime.From(_) otherwise null, type nullable datetime}}
+  ),
+  #"Filtered valid datetimes" = Table.SelectRows(#"Converted Binnen safely", each [Binnen geweest op] <> null)
 in
-  #"Changed column type"
+  #"Filtered valid datetimes"
